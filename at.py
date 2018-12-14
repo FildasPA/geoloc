de32e36b7259c3a75b027bc2e3af5eee10edac72
FildasPA <fildas.pa3@gmail.com>
2018-12-14 12:06:43 +0100

    update at

--- at.py
+++ at.py
@@ -5,44 +5,43 @@ ser = serial.Serial('/dev/ttyS0')
 ser.baudrate = 115200
 print(ser.name)
 
+def readline(ser):
+    res = ''
+    while True:
+        byte = ser.read()
+        if byte == '\r':
+            return res
+        res += byte
+
+def read_beacon_infos(ser):
+    infos = []
+    while True:
+        line = readline(ser)
+        if line == '':
+            return infos
+        print(line)
+        infos.append(line)
+
+def read_beacons(ser):
+    beacons_infos = []
+    while True:
+        infos = read_beacon_infos(ser)
+        if not infos:
+            return beacons_infos
+        beacons_infos.append(infos)
+
+
 while True:
     sleep(1)
-    #print('writing +++')
     ser.write('+++')
     sleep(1)
 
     incomingByte = ser.read(size=2);
-    # print('read something')
-    # print(incomingByte)
+
     if (incomingByte == 'OK'):
+        print('Initialized!')
         sleep(1)
-        # ser.write('ATIDC133\r')
         incomingByte = ''
         ser.write('ATND\r')
-        res = ''
-        eol = False
-        while True:
-            incomingByte = ser.read()
-            #print(repr('Lu: %s' % incomingByte))
-            if eol and incomingByte == '\r':
-                break
-            # if incomingByte == '\r' and res == '\r':
-            #     break
-            #print('res: %s' % repr(res))
 
-            if incomingByte == '\r':
-                print(repr(res))
-                res = ''
-                eol = True
-            else:
-                res += incomingByte
-                eol = False
-        print('fin de la boucle!')
-
-        #incomingByte = ser.read(size=10);
-        #print(incomingByte)
-        # sleep(1)
-        # ser.write('ATWR\r')
-        # sleep(1)
-        # ser.write('ATCN\r')
-
+        read_beacons_infos(ser)
