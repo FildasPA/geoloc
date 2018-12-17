dc13c0b833d3962cf546529414f03ee6ac39f6d5
FildasPA <fildas.pa3@gmail.com>
2018-12-17 15:16:17 +0100

    Update at

--- at.py
+++ at.py
@@ -1,9 +1,18 @@
 import serial
 from time import sleep
 
-ser = serial.Serial('/dev/ttyS0')
-ser.baudrate = 115200
+ser = serial.Serial('/dev/ttyS0', baudrate=115200)
+
+
 print(ser.name)
+
+finger = {}
+
+
+def add_infos(node_id, rssi):
+    if not finger[node_id]:
+        finger[node_id] = rssi
+
 
 def readline(ser):
     res = ''
@@ -15,13 +24,16 @@ def readline(ser):
         res += byte
 
 def read_beacon_infos(ser):
+    global finger
+
     infos = []
     # print('Reading beacon')
     while True:
         line = readline(ser)
         if line == '':
-            return infos
-        print(line)
+            add_infos(infos[-1], infos[-2])
+            return infos[-2:]
+        # print(line)
         infos.append(line)
 
 def read_beacons(ser):
@@ -29,23 +41,30 @@ def read_beacons(ser):
     # print('Reading beacons infos')
     while True:
         infos = read_beacon_infos(ser)
-        print('')
         if not infos:
             return beacons_infos
-        beacons_infos.append(infos)
+        beacon, rssi
+        beacons_infos.append({'beacons': infos[-1], 'rssi': infos[-2]})
 
 
-while True:
+def send():
     sleep(1)
     ser.write('+++')
     sleep(1)
-
-    incomingByte = ser.read(size=3);
-
+    incomingByte = ser.read(size=3)
     if (incomingByte == 'OK\r'):
         print('+++')
         sleep(1)
         incomingByte = ''
         ser.write('ATND\r')
+        print(read_beacons(ser))
+        # ser.write('ATCN\r')
 
-        read_beacons(ser)
+
+def main():
+    while True:
+        send()
+
+
+if __name__ == "__main__":
+    main()
