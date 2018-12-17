8744b82bb9a12b31408db4a5fb2567dcfec00aca
AdrienSa <adrien.sartori@alumni.univ-avignon.fr>
2018-12-17 15:31:33 +0100

    Update db.py

--- db.py
+++ db.py
@@ -8,18 +8,26 @@ class BDD:
     def __init__(self):
         """Initialise la connexion avec la BDD"""
         self.client = pymongo.MongoClient('mongodb://localhost:27017')
-        self.db = client['pymongo_test']
+        self.db = self.client.defi
 
 
     def __del__(self):
         # self.db.close() # Si besoin de fermer la connexion manuellement
         pass
 
-    def add_fingerprint(values):
+    def add_fingerprint(self, values):
         """values = {'x': N, 'y': N, 'BALISE_1': N, 'BALISE_2': N, ..., 'BALISE_5': N}"""
+        self.db.fingers.insert_one(values).inserted_id
+        print("Valeur ajoutée !")
         pass
 
 
-    def get_all_values():
+    def get_all_values(self):
         """Renvoie toutes les valeurs"""
+        self.db.fingers.find()
         pass
+
+b = BDD()
+values = {"x":"0","y":"0","BALISE_1":"0","BALISE_2":"0","BALISE_3":"0","BALISE_4":"0","BALISE_5":"0"}
+b.add_fingerprint(values)
+b.get_all_values()
