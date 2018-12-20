f02b041e6e3c868a3dd9b9462399c3b0bf9cfb76
FildasPA <fildas.pa3@gmail.com>
2018-12-20 19:31:25 +0100

    Set and update position on the server

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-#! /usr/bin/python
-# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
+x = 3
+y = 2
+
 @app.route('/')
 def index():
-     return render_template('app.html', bottom='133px', left='126px')
+    return render_template('app.html', x=x, y=y)
+
+
+@app.route('/setPosition')
+def setPosition():
+    global x, y
+    x = request.args.get('x')
+    y = request.args.get('y')
+    return ''
+
+
+@app.route('/getPosition')
+def getPosition():
+    return '{"x": %s, "y": %s}' % (x, y)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
