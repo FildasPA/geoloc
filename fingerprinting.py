7d2c3bc463d61cfa36e49533b78ff130373d429f
FildasPA <fildas.pa3@gmail.com>
2018-12-21 09:47:24 +0100

    Update fingerprinting
    - Rename db.insert_print to insert
    - set requests timeout
    - update ranges
    - uses while loops instead of for

--- fingerprinting.py
+++ fingerprinting.py
@@ -7,32 +7,46 @@ from db import DB
 import at
 
 
-max_x = 3
-max_y = 3
+max_x = 8
+max_y = 8
 
 db = DB()
 
 
 def insert_print(values):
-    # values['x'] = raw_input('x ? ')
-    # values['y'] = raw_input('y ? ')
+    db.insert(values)
 
-    db.insert_print(values)
 
+def record(x, y):
+    print('Doing: (%s, %s)' % (x, y))
+
+    params = {'x': x, 'y': y}
+    try:
+        requests.get(url='http://10.120.14.37:5000/setPosition', params=params, timeout=5)
+    except requests.exceptions.Timeout:
+        pass
+
+    at.get_fingerprint(insert_print, x, y)
 
 def main():
     global x, y
 
-    for y in range(max_y):
-        for x in range(max_x):
-            print('Doing: (%s, %s)' % (x, y))
+    x = 5
+    y = 2
 
-            params = {'x': x, 'y': y}
-            requests.get(url='http://10.120.14.37:5000/setPosition', params=params)
+    while x < max_x:
+        if x % 2 == 0:
+            while y < max_y:
+                record(x, y)
+                y += 1
+            y = max_y - 1
+        else:
+            while y >= 0:
+                record(x, y)
+                y -= 1
+            y = 0
+        x += 1
 
-            at.get_fingerprint(insert_print, x, y)
-
-            requests.get(url='http://10.120.13.52:5001')
 
 
 if __name__ == "__main__":
